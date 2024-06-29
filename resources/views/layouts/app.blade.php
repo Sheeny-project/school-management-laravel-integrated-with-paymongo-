b8255d799440324456a521d3a739a173ecc8da74
Sheeny-project <sheenrusselcastillo@gmail.com>
2024-06-29 11:15:52 +0800

    New change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,10 +73,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="/" class="brand-link">
-            <img src="{{ asset('images/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-                 class="brand-image img-circle elevation-3"
-                 style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <span class="brand-text font-weight-light">School Management</span>
         </a>
 
         @include('layouts.navigation')
@@ -101,11 +98,8 @@
     <!-- Main Footer -->
     <footer class="main-footer">
         <!-- To the right -->
-        <div class="float-right d-none d-sm-inline">
-            Anything you want
-        </div>
         <!-- Default to the left -->
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2014-2021 </strong> All rights reserved.
     </footer>
 </div>
 <!-- ./wrapper -->
